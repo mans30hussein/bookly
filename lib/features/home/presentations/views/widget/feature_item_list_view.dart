7a4e9a7590d19aab26c25f5bef818bee2433fb01
30102091900132 <mohamedhusseinmans@gmail.com>
2024-03-03 15:47:52 +0300

    add feature list view to home

---
+++ lib/features/home/presentations/views/widget/feature_item_list_view.dart
@@ -0,0 +1,26 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class FeatureItemListView extends StatelessWidget {
+  const FeatureItemListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4, // width : height
+        child: Container(
+         
+          decoration: BoxDecoration(
+            color: Colors.red,
+            
+            borderRadius: BorderRadius.circular(20),
+            //border:
+          ),
+          child: Image.asset(AssetsDate.test_Image , fit: BoxFit.contain,),
+        ),
+      ),
+    );
+  }
+}
