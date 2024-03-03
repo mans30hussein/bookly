c860d348d694b05d9959fe8488dcd4e5fff4c449
30102091900132 <mohamedhusseinmans@gmail.com>
2024-03-03 10:46:20 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+
+         //home: SplashViews(),
+        );
+  }
+}
